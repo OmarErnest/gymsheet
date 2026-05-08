29ade23955ef78444f70dd90ae36b04614fb413a
OmarErnest <omarecesin@gmail.com>
2026-05-08 15:11:34 -0400

    no pre-hash needed

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,7 +1,30 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import User, UserPreference
+
+
+class UserAdminForm(forms.ModelForm):
+    pin = forms.CharField(
+        max_length=6,
+        required=False,
+        widget=forms.PasswordInput(render_value=True),
+        help_text="Enter a 6-digit PIN to hash it automatically. Leave blank to keep current PIN."
+    )
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        pin = self.cleaned_data.get('pin')
+        if pin:
+            user.set_pin(pin)
+        if commit:
+            user.save()
+        return user
 
 
 @admin.action(description='Approve selected users')
@@ -31,14 +54,18 @@ def approve_profile_pics(modeladmin, request, queryset):
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
+    form = UserAdminForm
     list_display = ('email', 'name', 'gender', 'auth_mode', 'is_approved', 'is_staff', 'date_joined')
     list_filter = ('is_approved', 'gender', 'auth_mode', 'is_staff', 'is_superuser')
     search_fields = ('email', 'name')
     ordering = ('email',)
     actions = [approve_users, reset_dummy_data, approve_profile_pics]
     fieldsets = BaseUserAdmin.fieldsets + (
-        ('GymSheet profile', {'fields': ('name', 'profile_pic_url', 'profile_pic_url_pending', 'gender', 'is_approved', 'auth_mode', 'pin_hash')}),
+        ('GymSheet profile', {'fields': (
+            'name', 'profile_pic_url', 'profile_pic_url_pending', 'gender', 'is_approved', 'auth_mode', 'pin', 'pin_hash'
+        )}),
     )
+    readonly_fields = ('pin_hash',)
 
 
 @admin.register(UserPreference)
